092b0422ae3d3f791d20c496a2d6ce85b23b8609
ahmadarif111999-collab <ahmadarif111999@gmail.com>
2026-07-11 10:34:08 +0500

    Update accounting.module.ts

--- apps/api/src/modules/accounting/accounting.module.ts
+++ apps/api/src/modules/accounting/accounting.module.ts
@@ -3,12 +3,14 @@ import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { BusinessesModule } from '../businesses/businesses.module';
 import { AccountingController } from './accounting.controller';
+import { AccountingViewsController } from './accounting-views.controller';
 import { AccountingService } from './accounting.service';
+import { AccountingViewsService } from './accounting-views.service';
 
 @Module({
   imports: [AuthModule, PrismaModule, BusinessesModule],
-  controllers: [AccountingController],
-  providers: [AccountingService],
+  controllers: [AccountingController, AccountingViewsController],
+  providers: [AccountingService, AccountingViewsService],
   exports: [AccountingService],
 })
 export class AccountingModule {}
