08b183e21a792b0f2030dd4082f09b6bce67b658
ahmadarif111999-collab <ahmadarif111999@gmail.com>
2026-07-11 12:32:27 +0500

    Update accounting.module.ts

--- apps/api/src/modules/accounting/accounting.module.ts
+++ apps/api/src/modules/accounting/accounting.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { BusinessesModule } from '../businesses/businesses.module';
+import { PeriodsModule } from '../periods/periods.module';
 import { AccountingController } from './accounting.controller';
 import { AccountingViewsController } from './accounting-views.controller';
 import { AccountingReportingController } from './accounting-reporting.controller';
@@ -10,7 +11,7 @@ import { AccountingViewsService } from './accounting-views.service';
 import { AccountingReportingService } from './accounting-reporting.service';
 
 @Module({
-  imports: [AuthModule, PrismaModule, BusinessesModule],
+  imports: [AuthModule, PrismaModule, BusinessesModule, PeriodsModule],
   controllers: [AccountingController, AccountingViewsController, AccountingReportingController],
   providers: [AccountingService, AccountingViewsService, AccountingReportingService],
   exports: [AccountingService],
