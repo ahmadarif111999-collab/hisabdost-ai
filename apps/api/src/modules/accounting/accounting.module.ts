72b860c82420a7daa1a60963933ebe5054d0eac6
ahmadarif111999-collab <ahmadarif111999@gmail.com>
2026-07-13 09:36:25 +0500

    Update accounting.module.ts

--- apps/api/src/modules/accounting/accounting.module.ts
+++ apps/api/src/modules/accounting/accounting.module.ts
@@ -6,14 +6,26 @@ import { PeriodsModule } from '../periods/periods.module';
 import { AccountingController } from './accounting.controller';
 import { AccountingViewsController } from './accounting-views.controller';
 import { AccountingReportingController } from './accounting-reporting.controller';
+import { FinancialStatementsController } from './financial-statements.controller';
 import { AccountingService } from './accounting.service';
 import { AccountingViewsService } from './accounting-views.service';
 import { AccountingReportingService } from './accounting-reporting.service';
+import { FinancialStatementsService } from './financial-statements.service';
 
 @Module({
   imports: [AuthModule, PrismaModule, BusinessesModule, PeriodsModule],
-  controllers: [AccountingController, AccountingViewsController, AccountingReportingController],
-  providers: [AccountingService, AccountingViewsService, AccountingReportingService],
+  controllers: [
+    AccountingController,
+    AccountingViewsController,
+    AccountingReportingController,
+    FinancialStatementsController,
+  ],
+  providers: [
+    AccountingService,
+    AccountingViewsService,
+    AccountingReportingService,
+    FinancialStatementsService,
+  ],
   exports: [AccountingService],
 })
 export class AccountingModule {}
