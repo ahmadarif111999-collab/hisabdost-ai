63483e79361340e6565960a3d82780a3de72a210
ahmadarif111999-collab <ahmadarif111999@gmail.com>
2026-07-14 14:15:14 +0500

    Update accounting.module.ts

--- apps/api/src/modules/accounting/accounting.module.ts
+++ apps/api/src/modules/accounting/accounting.module.ts
@@ -3,19 +3,27 @@ import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { BusinessesModule } from '../businesses/businesses.module';
 import { PeriodsModule } from '../periods/periods.module';
+
 import { AccountingController } from './accounting.controller';
 import { AccountingViewsController } from './accounting-views.controller';
 import { AccountingReportingController } from './accounting-reporting.controller';
 import { FinancialStatementsController } from './financial-statements.controller';
 import { XlsxExportController } from './xlsx-export.controller';
+
 import { AccountingService } from './accounting.service';
 import { AccountingViewsService } from './accounting-views.service';
 import { AccountingReportingService } from './accounting-reporting.service';
 import { FinancialStatementsService } from './financial-statements.service';
 import { XlsxExportService } from './xlsx-export.service';
+import { ReportApprovalService } from './report-approval.service';
 
 @Module({
-  imports: [AuthModule, PrismaModule, BusinessesModule, PeriodsModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    BusinessesModule,
+    PeriodsModule,
+  ],
   controllers: [
     AccountingController,
     AccountingViewsController,
@@ -29,7 +37,10 @@ import { XlsxExportService } from './xlsx-export.service';
     AccountingReportingService,
     FinancialStatementsService,
     XlsxExportService,
+    ReportApprovalService,
   ],
-  exports: [AccountingService],
+  exports: [
+    AccountingService,
+  ],
 })
 export class AccountingModule {}
