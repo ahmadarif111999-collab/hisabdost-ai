f073fee944b662d94cd966c77397b9415c7f9408
ahmadarif111999-collab <ahmadarif111999@gmail.com>
2026-07-25 14:37:38 +0500

    Update reference-numbers.module.ts

--- apps/api/src/modules/reference-numbers/reference-numbers.module.ts
+++ apps/api/src/modules/reference-numbers/reference-numbers.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
+import { BusinessesModule } from '../businesses/businesses.module';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { PaymentActivityService } from './payment-activity.service';
+import { ReferenceNumbersController } from './reference-numbers.controller';
 import { ReferenceNumbersService } from './reference-numbers.service';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [ReferenceNumbersService],
-  exports: [ReferenceNumbersService],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    BusinessesModule,
+  ],
+  controllers: [
+    ReferenceNumbersController,
+  ],
+  providers: [
+    ReferenceNumbersService,
+    PaymentActivityService,
+  ],
+  exports: [
+    ReferenceNumbersService,
+  ],
 })
 export class ReferenceNumbersModule {}
