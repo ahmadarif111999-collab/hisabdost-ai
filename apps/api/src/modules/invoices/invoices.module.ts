c517a943526590c7ed0c1513d9abac8be9b9bd70
ahmadarif111999-collab <ahmadarif111999@gmail.com>
2026-07-25 13:25:38 +0500

    Update invoices.module.ts

--- apps/api/src/modules/invoices/invoices.module.ts
+++ apps/api/src/modules/invoices/invoices.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
-import { PrismaModule } from '../../prisma/prisma.module';
 import { BusinessesModule } from '../businesses/businesses.module';
 import { PeriodsModule } from '../periods/periods.module';
+import { ReferenceNumbersModule } from '../reference-numbers/reference-numbers.module';
+import { PrismaModule } from '../../prisma/prisma.module';
 import { InvoicesController } from './invoices.controller';
 import { InvoicesService } from './invoices.service';
 
 @Module({
-  imports: [AuthModule, PrismaModule, BusinessesModule, PeriodsModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    BusinessesModule,
+    PeriodsModule,
+    ReferenceNumbersModule,
+  ],
   controllers: [InvoicesController],
   providers: [InvoicesService],
 })
