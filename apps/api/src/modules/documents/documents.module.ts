c42f6ebb642d2461e67570be88f33aad8740c687
ahmadarif111999-collab <ahmadarif111999@gmail.com>
2026-08-08 10:31:38 +0500

    Update documents.module.ts

--- apps/api/src/modules/documents/documents.module.ts
+++ apps/api/src/modules/documents/documents.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { AccountingModule } from '../accounting/accounting.module';
+import { AuthModule } from '../auth/auth.module';
 import { BusinessesModule } from '../businesses/businesses.module';
 import { OcrModule } from '../ocr/ocr.module';
+import { ReferenceNumbersModule } from '../reference-numbers/reference-numbers.module';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
 
 @Module({
-  imports: [AuthModule, PrismaModule, BusinessesModule, OcrModule, AccountingModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    AccountingModule,
+    BusinessesModule,
+    OcrModule,
+    ReferenceNumbersModule,
+  ],
   controllers: [DocumentsController],
   providers: [DocumentsService],
+  exports: [DocumentsService],
 })
 export class DocumentsModule {}
