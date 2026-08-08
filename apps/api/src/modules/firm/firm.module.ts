2e8767e1490a9bbc825503b5b2e9d597db319745
ahmadarif111999-collab <ahmadarif111999@gmail.com>
2026-08-08 10:32:08 +0500

    Update firm.module.ts

--- apps/api/src/modules/firm/firm.module.ts
+++ apps/api/src/modules/firm/firm.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { AuthModule } from '../auth/auth.module';
 import { BusinessesModule } from '../businesses/businesses.module';
+import { ReferenceNumbersModule } from '../reference-numbers/reference-numbers.module';
 import { FirmController } from './firm.controller';
 import { FirmService } from './firm.service';
 
 @Module({
-  imports: [AuthModule, PrismaModule, BusinessesModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    BusinessesModule,
+    ReferenceNumbersModule,
+  ],
   controllers: [FirmController],
   providers: [FirmService],
+  exports: [FirmService],
 })
 export class FirmModule {}
