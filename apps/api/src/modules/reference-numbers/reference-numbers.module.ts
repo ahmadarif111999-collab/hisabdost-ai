0e660beb8a5e3a125df391b560e078e158c8cad3
ahmadarif111999-collab <ahmadarif111999@gmail.com>
2026-08-10 10:18:59 +0500

    Update reference-numbers.module.ts

--- apps/api/src/modules/reference-numbers/reference-numbers.module.ts
+++ apps/api/src/modules/reference-numbers/reference-numbers.module.ts
@@ -1,35 +1,68 @@
-import { Global, Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import {
+  Global,
+  Module,
+} from '@nestjs/common';
+
+import {
+  APP_INTERCEPTOR,
+} from '@nestjs/core';
+
 import { PrismaModule } from '../../prisma/prisma.module';
+
 import { AuthModule } from '../auth/auth.module';
+
 import { BusinessesModule } from '../businesses/businesses.module';
+
+import { ExpensePurchaseRegistersController } from './expense-purchase-registers.controller';
+
+import { ExpensePurchaseRegistersService } from './expense-purchase-registers.service';
+
 import { HumanReadableReferenceInterceptor } from './human-readable-reference.interceptor';
+
 import { PaymentActivityService } from './payment-activity.service';
+
 import { ReferenceNumbersController } from './reference-numbers.controller';
+
 import { ReferenceNumbersService } from './reference-numbers.service';
+
 import { ReferencePresentationService } from './reference-presentation.service';
+
 import { ReferenceWorkflowController } from './reference-workflow.controller';
 
 @Global()
 @Module({
-  imports: [AuthModule, PrismaModule, BusinessesModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    BusinessesModule,
+  ],
+
   controllers: [
     ReferenceNumbersController,
     ReferenceWorkflowController,
+    ExpensePurchaseRegistersController,
   ],
+
   providers: [
     ReferenceNumbersService,
     ReferencePresentationService,
     PaymentActivityService,
+    ExpensePurchaseRegistersService,
+
     {
-      provide: APP_INTERCEPTOR,
-      useClass: HumanReadableReferenceInterceptor,
+      provide:
+        APP_INTERCEPTOR,
+
+      useClass:
+        HumanReadableReferenceInterceptor,
     },
   ],
+
   exports: [
     ReferenceNumbersService,
     ReferencePresentationService,
     PaymentActivityService,
+    ExpensePurchaseRegistersService,
   ],
 })
 export class ReferenceNumbersModule {}
