41055fe7076d979811e5026d56e781ca0fb4218a
ahmadarif111999-collab <ahmadarif111999@gmail.com>
2026-08-10 10:22:35 +0500

    Create page.tsx

---
+++ apps/web/app/purchases/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { TransactionRegisterPage } from '@/components/TransactionRegisterPage';
+
+export default function PurchasesPage() {
+  return (
+    <TransactionRegisterPage kind="purchase" />
+  );
+}
